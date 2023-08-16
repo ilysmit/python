eb9e514cbcb3101fcdfea98a4f5436f2077ebde7
smit <itsmit6879@gmail.com>
2023-08-17 01:26:21 +0530

    add loop file

---
+++ loop.py
@@ -0,0 +1,21 @@
+# simple while loop program
+
+    # i=1
+    # while i<=100:
+    #         print(i)
+    #         i+=1
+
+# while loop with breck statment
+
+i=1
+while i<=100:
+        print(i)
+        if (i==50):
+            break
+        i+=1
+
+
+
+
+
+    
