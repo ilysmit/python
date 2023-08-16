ced31bad085eaba87a8ae575d8f3b8b2ed49a9b9
smit <itsmit6879@gmail.com>
2023-08-17 00:42:24 +0530

    adding cls file

---
+++ cls.py
@@ -0,0 +1,25 @@
+num1=int(input("enter the first number :-  "))
+operator=input("enter operator \n (+ , * , - , / ) :- ")
+num2=int(input("enter the second number :-  "))
+
+if operator=="+":
+    
+        print(num1+num2)
+    
+elif operator=="-":
+    
+        print(num1-num2)
+    
+elif operator=="*":
+    
+        print(num1*num2)
+    
+elif operator=="/":
+
+        print(num1*num2)
+    
+else:
+        print("enter operator first !!!!!!")
+    
+
+    
