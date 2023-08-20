26de0b08f4aab03f6c631643d6fa9c0c9cc9cf88
smit <itsmit6879@gmail.com>
2023-08-21 02:32:52 +0530

    add sorting list

---
+++ mark_sort_list.py
@@ -0,0 +1,10 @@
+m1=int(input("enter mark of studant 1 :- "))
+m2=int(input("enter mark of studant 2 :- "))
+m3=int(input("enter mark of studant 3 :- "))
+m4=int(input("enter mark of studant 4 :- "))
+m5=int(input("enter mark of studant 5 :- "))
+m6=int(input("enter mark of studant 6 :- "))
+
+studantmark=[m1,m2,m3,m4,m5,m6]
+studantmark.sort()
+print(studantmark)
