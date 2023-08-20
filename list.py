39ef9409b49f142ae00be1f33c3d340a083fc654
smit <itsmit6879@gmail.com>
2023-08-20 15:25:54 +0530

    adding list.py

---
+++ list.py
@@ -0,0 +1,19 @@
+# marks=[90,95,65,566]
+# for i in marks:
+#     print(i)
+
+# marks.insert(0 ,99)
+# print(marks)
+
+studant=["radha","kishan","gori","ram","shyam"]
+
+for i in studant:
+    if i=="ram":
+        break;
+    print(i)
+
+
+for i in studant:
+    if i=="ram" :
+        continue;
+    print(i)
